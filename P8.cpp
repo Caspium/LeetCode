3944680cb1fc1393f8ec2e2ebb8cbd8a89aa123d
Caspium <caspium@tamu.edu>
2022-12-26 21:10:52 -0600

    problem 8

--- P8.cpp
+++ P8.cpp
@@ -10,27 +10,50 @@ public:
     {
         int mult = 1;
         int out = 0;
+        bool char_encounter = false;
         for (int i = 0; i < s.length(); i++)
         {
-            if (s[i] == ' ' || s[i] == '+')
+            if (char_encounter && !('0' <= s[i] && s[i] <= '9'))
+                return out;
+            if (s[i] == ' ')
                 continue;
+            if (s[i] == '+')
+            {
+                char_encounter = true;
+                continue;
+            }
             else if (s[i] == '-')
             {
                 mult = -1;
+                char_encounter = true;
                 continue;
             }
             else if ('0' <= s[i] && s[i] <= '9')
             {
-                if (mult * out > INT_MAX / 10)
+                char_encounter = true;
+                if (out > INT_MAX / 10)
                     return INT_MAX;
-                else if (mult * out < INT_MIN / 10)
+                else if (out < INT_MIN / 10)
                     return INT_MIN;
                 out *= 10;
-                out += s[i] - '0';
+                if (out > INT_MAX - (s[i] - '0'))
+                    return INT_MAX;
+                else if (out < INT_MIN + (s[i] - '0'))
+                    return INT_MIN;
+                out += (s[i] - '0') * mult;
             }
             else
                 break;
         }
-        return out * mult;
+        return out;
     }
 };
+
+int main()
+{
+    Solution s;
+    string x = "-38";
+    int result = s.myAtoi(x);
+    cout << result << endl;
+    return 0;
+}
